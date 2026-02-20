af8d3e3dec4f4db40b561fc0405a4ac378133277
GiulianoComoglio <122049426+GiulianoComoglio@users.noreply.github.com>
2026-02-20 18:45:11 +0100

    Add infinite loop to update text content

---
+++ script.js
@@ -0,0 +1,8 @@
+while(1) {
+    setTimeout(() => {
+        document.getElementById('tentativo').textContent="ciao"
+    }, 2000);
+    setTimeout(() => {
+            document.getElementById('tentativo').textContent="arrivederci"
+    }, 2000);
+}
