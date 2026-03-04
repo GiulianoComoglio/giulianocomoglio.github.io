df772d610e08490f889973560711ffe83b1a5b7b
GiulianoComoglio <comogliogiuliano@gmail.com>
2026-03-04 11:21:30 +0100

    new js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ let div1 = document.getElementById('div1');
 div1.innerHTML = "prova";
 
 while(1) {
-    setTimeout(
+    setInterval(
         () => {
             div1.innerHTML = "fatto"
         }, 2000
     );
-    setTimeout(
+    setInterval(
         () => {
             div1.innerHTML = "prova"
         }, 2000
