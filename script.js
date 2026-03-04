b95a6a6cd9cc6dde491e26488591f21a69eeebe1
GiulianoComoglio <comogliogiuliano@gmail.com>
2026-03-04 23:59:59 +0100

    new js

--- script.js
+++ script.js
@@ -6,12 +6,11 @@ let count = 0;
 
 let text = texts[count];
 
-div1.innerHTML = text;
-
 while (1) {
     setInterval(
         () => {
             count = !count;
         }, 1000
-    )
+    );
+    div1.innerHTML = text;
 };
