458f060dd7fc0558f37e5a91e420740f1c974ec1
GiulianoComoglio <comogliogiuliano@gmail.com>
2026-03-10 17:41:16 +0100

    new js with audio test

--- script.js
+++ script.js
@@ -4,10 +4,21 @@ let texts = ["prova", "fatto"];
 
 let count = 0;
 
+let audio = new Audio(`https://cdn.freesound.org/previews/504/504819_9961300-lq.mp3`);
+
+audio.volume(0.1);
+
+
 window.onload = setInterval(
     () => {
         count += 1;
         count = count % 2; 
         div1.innerHTML = texts[count];
+        if (count == 0) {
+            audio.play();
+        } else {
+            audio.pause(); 
+        };
     }, 1000
 );
+
